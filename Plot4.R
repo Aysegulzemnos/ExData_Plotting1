45cda5470aaf6a243c8084cba7e89b6600e4e6f5
Aysegulzemnos <36179859+Aysegulzemnos@users.noreply.github.com>
2018-02-16 19:10:29 -0500

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -6,16 +6,23 @@ plot4 <- function(data=NULL) {
 
     png("plot4.png", width=480, height=480)
 
+##Time series plots of global active power, global reactive power, voltage and sub_metering
+## Split the device into four plotting regions
     par(mfrow=c(2,2))
-    ## 1
+  
+    ## Create plots and send to file
+    ## Create the 'Global Active Power' vs 'Time' plot
+    
     plot(data$Time, data$Global_active_power,
          type="l",
          xlab="",
          ylab="Global Active Power")
-    ## 2
-    plot(data$Time, data$Voltage, type="l",
+    
+    ## Create the 'Voltage' vs 'Time' plot
+      plot(data$Time, data$Voltage, type="l",
          xlab="datetime", ylab="Voltage")
-    ## 3
+    
+    ## Create the 'Energy sub metering' vs 'Time' plot
     plot(data$Time, data$Sub_metering_1, type="l", col="black",
          xlab="", ylab="Energy sub metering")
     lines(data$Time, data$Sub_metering_2, col="red")
@@ -25,7 +32,9 @@ plot4 <- function(data=NULL) {
            c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
            lty=1,
            box.lwd=0)
-    ## 4
+    
+    ## Create the 'Global_reactive_power' vs 'Time' plot
+    
     plot(data$Time, data$Global_reactive_power, type="n",
          xlab="datetime", ylab="Global_reactive_power")
     lines(data$Time, data$Global_reactive_power)
